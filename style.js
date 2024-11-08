02fe49690404e288432cc276316eba36de88a1d9
baophi852@gmail.com <baophi852@gmail.com>
2024-11-08 11:15:01 +0700

    demo cardd

---
+++ style.js
@@ -0,0 +1,28 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '#splide-demo',{
+        perPage: 5,
+        rewind : true,
+        type: "loop",
+        perMove: 1,
+        wheel: true,
+        autoplay: true,
+        breakpoints: {
+            1320: {
+              perPage: 4,
+            },
+            1058: {
+              perPage: 3,
+            },
+            796: {
+                perPage: 2,
+            },
+            534: {
+                perPage: 1,
+            },
+
+          },
+    } );
+    splide.mount(); /*vận hành, hiển thị*/
+} );
+
+
